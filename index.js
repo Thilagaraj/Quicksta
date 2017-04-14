c09d0b7c67f9ba9dcf856e0423366dfde719f103
Thilagaraj <tk.thilagaraj@gmail.com>
2017-04-14 21:10:52 +0530

    .

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ app.use(function(req, res, next) {
   next();
 });
 app.get('/search/user/:q', function(request, response) {
-		
-	var session = new Client.Session(device, storage);		
-	var feed = new Client.Account.search(session, request.params.q);
-
+	var session = new Client.Session(device, storage);
+	var feed = new Client.Account.search(session, request.params.q);	
 	feed.then(function(results) {			
 		var searchList=_.map(results,function(list){
 			return list._params;
@@ -24,7 +22,22 @@ app.get('/search/user/:q', function(request, response) {
 		response.send(searchList);
 	})
 });
-
+app.get('/user/media/:userid', function(request, response) {
+		
+	var session = new Client.Session(device, storage);		
+	var feed = new Client.Feed.UserMedia(session, request.params.userid);
+	feed.get().then(function(results) {			
+		var searchList=_.map(results,function(list){
+			return list._params;
+		});
+		new Client.Account.getById(session, request.params.userid)
+		  .then(function(account) {
+			var accountParams=account._params;
+			response.send({"postList":searchList,"userInfo":accountParams});
+		  })
+		
+	})
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
